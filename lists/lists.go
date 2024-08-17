979202c5bf29803b2dfc1d034d516e151bb0dca7
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-08-17 14:53:02 +0400

    added list

---
+++ lists/lists.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	var intList list.List
+	intList.PushBack(11)
+	intList.PushFront(23)
+	intList.PushBack(34)
+
+	for element := intList.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value.(int))
+	}
+
+}
